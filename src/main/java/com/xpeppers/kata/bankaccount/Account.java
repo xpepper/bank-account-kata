47d81fe25c7877ba24bdf66c345850a8066a9af7
Pietro Di Bello <pierodibello@gmail.com>
2018-04-15 22:30:35 +0200

    have the acceptance test failing for the right reason

--- src/main/java/com/xpeppers/kata/bankaccount/Account.java
+++ src/main/java/com/xpeppers/kata/bankaccount/Account.java
@@ -2,14 +2,11 @@ package com.xpeppers.kata.bankaccount;
 
 public class Account {
     public void deposit(int amount) {
-        throw new UnsupportedOperationException();
     }
 
     public void withdrawal(int amount) {
-        throw new UnsupportedOperationException();
     }
 
     public void printStatement() {
-        throw new UnsupportedOperationException();
     }
 }
