5904d57eaee42ec591946f5f45d5a6e7f79a0374
Pietro Di Bello <pierodibello@gmail.com>
2018-04-15 22:25:37 +0200

    have the first (failing) acceptance test in place

---
+++ src/main/java/com/xpeppers/kata/bankaccount/Account.java
@@ -0,0 +1,15 @@
+package com.xpeppers.kata.bankaccount;
+
+public class Account {
+    public void deposit(int amount) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void withdrawal(int amount) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void printStatement() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/com/xpeppers/kata/bankaccount/Console.java
@@ -0,0 +1,7 @@
+package com.xpeppers.kata.bankaccount;
+
+public class Console {
+    public void printLine(String text) {
+        throw new UnsupportedOperationException();
+    }
+}
