9946e2e82c4a7dd48ac2f9ce7e0c775f71e48eb4
Pietro Di Bello <pierodibello@gmail.com>
2018-04-17 20:15:25 +0200

    improve acceptance test specifying the exact order of the call to console

--- src/test/java/feature/PrintStatementFeature.java
+++ src/test/java/feature/PrintStatementFeature.java
@@ -5,9 +5,11 @@ import com.xpeppers.kata.bankaccount.Console;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -26,17 +28,16 @@ public class PrintStatementFeature {
     @Test
     public void
     print_statement_containing_all_transactions() {
-
         account.deposit(1000);
         account.withdrawal(100);
         account.deposit(500);
 
         account.printStatement();
 
-        verify(console).printLine("DATE | AMOUNT | BALANCE");
-        verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
-        verify(console).printLine("02/04/2014 | -100.00 | 900.00");
-        verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
-
+        InOrder inOrder = inOrder(console);
+        inOrder.verify(console).printLine("DATE | AMOUNT | BALANCE");
+        inOrder.verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
+        inOrder.verify(console).printLine("02/04/2014 | -100.00 | 900.00");
+        inOrder.verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
     }
 }
